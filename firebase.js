5dd269d71f1c7a39357808f003f5482837786802
Kirill Ostapenko <djkroq@gmail.com>
2021-06-09 18:14:35 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA95OrcHoSlhpvrAWsEmFUehVs_qN9fj_A",
-    authDomain: "next-storage-f5c62.firebaseapp.com",
-    projectId: "next-storage-f5c62",
-    storageBucket: "next-storage-f5c62.appspot.com",
-    messagingSenderId: "862630774392",
-    appId: "1:862630774392:web:8fda87686004afad1b4f8d",
-    measurementId: "G-MR6262BH9D"
+    apiKey: "add",
+    authDomain: "add",
+    projectId: "add",
+    storageBucket: "add",
+    messagingSenderId: "add",
+    appId: "add",
+    measurementId: "add"
   };
 
 const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
